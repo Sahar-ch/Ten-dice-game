d3ae20b941e99cc97db8eb927b815e36384f1d76
Sahar-ch <sahar68.chegeni@gmail.com>
2022-10-21 10:34:01 -0400

    gameplay implemented

--- dice/src/App.js
+++ dice/src/App.js
@@ -5,19 +5,59 @@ const App = () => {
   const createNumbers = () => {
     const numbers = [];
     for (let i = 0; i < 10; i++) {
-      numbers.push(Math.ceil(Math.random() * 6));
+      numbers.push({
+        id: i,
+        selected: false,
+        number: Math.ceil(Math.random() * 6),
+      });
     }
     return numbers;
   };
 
   const [randomNumbers, setRandomNumbers] = useState(createNumbers());
 
-  const dice = randomNumbers.map((number) => {
-    return <Die value={number} />;
+  const selectHandler = (id) => {
+    setRandomNumbers((prevState) => {
+      return prevState.map((die) => {
+        return die.id === id ? { ...die, selected: !die.selected } : die;
+      });
+    });
+  };
+
+  const dice = randomNumbers.map((obj) => {
+    return (
+      <Die
+        value={obj.number}
+        isSelected={obj.selected}
+        key={obj.id}
+        id={obj.id}
+        onSelectHandler={selectHandler}
+      />
+    );
   });
 
   const rollHandler = () => {
-    setRandomNumbers(createNumbers());
+    setRandomNumbers((prevState) => {
+      return prevState.map((die) => {
+        // let result;
+        // if (die.selected) {
+        //   result = die;
+        // } else {
+        //   result = {
+        //     ...die,
+        //     number: Math.ceil(Math.random() * 6),
+        //   };
+        // }
+        // return result;
+
+        return die.selected
+          ? die
+          : {
+              ...die,
+              number: Math.ceil(Math.random() * 6),
+            };
+      });
+    });
   };
   return (
     <main>
--- dice/src/components/Die.js
+++ dice/src/components/Die.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Die = (props) => {
   return (
-    <div className="die-front">
+    <div
+      className={props.isSelected ? "die-front selected" : "die-front"}
+      onClick={(event) => props.onSelectHandler(props.id)}
+    >
       <h2 className="text">{props.value}</h2>
     </div>
   );
